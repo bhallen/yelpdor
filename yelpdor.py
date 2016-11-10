2aa1b055d8937935df81808f8845de7b846207f3
Lorin Beer <lorin.beer@gmail.com>
2016-11-10 10:55:03 -0800

    basic map and object

--- yelpdor.py
+++ yelpdor.py
@@ -10,6 +10,47 @@ libtcod.console_put_char(0, 1, 1, '@', libtcod.BKGND_NONE)
 libtcod.console_flush()
 
 
+color_dark_wall = libtcod.Color(0, 0, 100)
+color_dark_ground = libtcod.Color(50, 50, 150)
+
+class Tile:
+    #a tile of the map and its properties
+    def __init__(self, blocked, block_sight = None):
+        self.blocked = blocked
+ 
+        #by default, if a tile is blocked, it also blocks sight
+        if block_sight is None: block_sight = blocked
+        self.block_sight = block_sight
+
+
+def make_map():
+    global map
+ 
+    #fill map with "unblocked" tiles
+    map = [[ Tile(False)
+        for y in range(MAP_HEIGHT) ]
+            for x in range(MAP_WIDTH) ]
+
+ map[30][22].blocked = True
+    map[30][22].block_sight = True
+    map[50][22].blocked = True
+    map[50][22].block_sight = True
+
+def render_all():
+    #draw all objects in the list
+    for object in objects:
+        object.draw()
+        for y in range(MAP_HEIGHT):
+                for x in range(MAP_WIDTH):
+                    wall = map[x][y].block_sight
+                    if wall:
+                        libtcod.console_set_char_background(con, x, y, color_dark_wall, libtcod.BKGND_SET )
+                    else:
+                        libtcod.console_set_char_background(con, x, y, color_dark_ground, libtcod.BKGND_SET )
+
+
+
+
 while not libtcod.console_is_window_closed():
   key = libtcod.console_wait_for_keypress(True)
 
---
+++ yelpdor/object.py
@@ -0,0 +1,22 @@
+class Object:
+    #this is a generic object: the player, a monster, an item, the stairs...
+    #it's always represented by a character on screen.
+    def __init__(self, x, y, char, color):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+ 
+    def move(self, dx, dy):
+        #move by the given amount
+        self.x += dx
+        self.y += dy
+ 
+    def draw(self):
+        #set the color and then draw the character that represents this object at its position
+        libtcod.console_set_default_foreground(con, self.color)
+        libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+ 
+    def clear(self):
+        #erase the character that represents this object
+        libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
