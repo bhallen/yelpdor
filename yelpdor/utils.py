9a0a5166dba49c8bb8eef6f13cf4c1d0f867341b
blake <blake@yelp.com>
2016-11-11 11:35:43 -0800

    move bizname generation data to json files

--- yelpdor/utils.py
+++ yelpdor/utils.py
@@ -6,3 +6,8 @@ def load_eventset(name):
     path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'res', 'events', name + '.json')
     fp = open(path).read()
     return json.loads(fp)
+
+def load_name_recipes(name):
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'res', 'events', name + '.json')
+    fp = open(path).read()
+    return json.loads(fp)
